62f68f0e5531921af98880c50ae2ab568388cd58
Alice.Dai <qingedaig@gmail.com>
2023-06-06 23:20:16 +1000

    add header, etc

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function AboutPage() {
+    return (
+        <div>
+            <Header />
+            <h1>About</h1>
+            <p>This is the about page.</p>
+        </div>
+    );
+}
+
+export default AboutPage;
